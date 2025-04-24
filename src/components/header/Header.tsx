975f70552720871defe41e7fe6fa91fdc0b642fa
smuke <smuke@smuke.dev>
2025-04-23 18:08:01 -0700

    Update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,17 @@
+import Container from "../container/Container";
+import Logo from "../logo/Logo";
 import classes from "./Header.module.css";
 
 function Header() {
     return (
-        <div className={classes.header}>Test Header</div>
+        <div className={classes.header}>
+            <Container>
+                <div className={classes.headerLayout}>
+                    <Logo />
+                    Profile
+                </div>
+            </Container>
+        </div>
     )
 }
 
