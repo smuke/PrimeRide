816d2dc214963647418b2e478b75a02f7bb0efd8
smuke <smuke@smuke.dev>
2025-04-24 17:11:39 -0700

    Update discover

--- src/pages/discover/Discover.tsx
+++ src/pages/discover/Discover.tsx
@@ -1,3 +1,5 @@
+import Header from "../../components/header/Header";
+import Container from "../../components/container/Container";
 import Card from "../../components/Card/Card";
 import classes from "./Discover.module.css";
 
@@ -7,22 +9,31 @@ function Discover() {
             <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
             <link href='https://fonts.googleapis.com/css?family=Instrument Sans' rel='stylesheet'></link>
 
-            <input className={classes.search_bar} placeholder="Enter a Location"/>
-            <div className={classes.center_part}>
-                <h1>Cars available near you</h1>
-                <div className={classes.buttons}>
-                    <button className={classes.filter_buttons}>Filter</button>
-                    <button className={classes.filter_buttons}>Sort by</button>
-                </div>
-            </div>
- 
-            <div className={classes.car_list}>
-                <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
-                <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
-                <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
-                <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
-            </div>
+            <Header />
+            
+            <section className={classes.section}>
+                <Container>
+                    <input className={classes.search_bar} placeholder="Enter a Location"/>
+                </Container>
+            </section>
 
+            <section className={classes.section}>
+                <Container>
+                    <div className={classes.center_part}>
+                        <h1>Cars available near you</h1>
+                        <div className={classes.buttons}>
+                            <button className={classes.filter_buttons}>Filter</button>
+                            <button className={classes.filter_buttons}>Sort by</button>
+                        </div>
+                    </div>
+                    <div className={classes.car_list}>
+                        <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
+                        <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
+                        <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
+                        <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
+                    </div>
+                </Container>
+            </section>
         </>
     )
 }
