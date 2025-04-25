7734bdf0533c3b47ab1332a4c2ef7e0d80c5c20d
smuke <smuke@smuke.dev>
2025-04-24 17:28:43 -0700

    Update current

--- src/pages/current/Current.tsx
+++ src/pages/current/Current.tsx
@@ -1,14 +1,18 @@
 import classes from "./Current.module.css";
+import Header from "../../components/header/Header";
+import Container from "../../components/container/Container";
 import Card from "../../components/card/Card";
 
 function Current() {
-
-    return(
+    return (
         <>
-        <h1>Current Rentals</h1>
-        <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
-        <h1>Past Rentals</h1>
-        <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
+            <Header />
+            <Container>
+                <h1>Current Rentals</h1>
+                <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
+                <h1>Past Rentals</h1>
+                <Card name="2022 Nissan Versa" pricePerDay={40.0} distance={3.2} rating={4.9} />
+            </Container>
         </>
     )
 }
