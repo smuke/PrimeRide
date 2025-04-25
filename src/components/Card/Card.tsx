de958457df70f28842789c827c047f9ab993efe6
ntvo110 <112120852+ntvo110@users.noreply.github.com>
2025-04-24 17:03:34 -0700

    pop up for clicking card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,13 @@
 import classes from "./Card.module.css";
 
+const handleCheckout = () => {
+    alert("The car rental has been bought")
+}
 
 function Card (props) {
     return (
         <>
-                <div className={classes.car}>
+                <div className={classes.car} onClick={handleCheckout}>
                     <img src="src\images\2022-nissan-versa.jpg" className={classes.car_img}></img>
                     <h3 className={classes.car_title}>{props.name}</h3>
                     <p className={classes.car_price}>${props.pricePerDay}/day</p>
