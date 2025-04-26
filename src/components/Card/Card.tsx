4e8e350c6e7f90c6331017e0eeb402a8abed4006
smuke <smuke@smuke.dev>
2025-04-26 14:18:03 -0700

    Update card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -17,9 +17,15 @@ function Card(props: CardProps) {
                 <div className={classes.car} onClick={handleCheckout}>
                     <img src="src\images\2022-nissan-versa.jpg" className={classes.car_img}></img>
                     <h3 className={classes.car_title}>{props.name}</h3>
-                    <p className={classes.car_price}>${props.pricePerDay}/day</p>
-                    <p className={classes.car_distance}>• {props.distance} mi away</p>
-                    <p className={classes.car_rating}>{props.rating}</p>
+                    <div className={classes.car_details}>
+                        <p>
+                            <span className={classes.car_price}>
+                                ${props.pricePerDay}/day
+                            </span>
+                             • {props.distance} mi away
+                        </p>
+                        <p className={classes.car_rating}>{props.rating}</p>
+                    </div>
                 </div>
         </>
     )
