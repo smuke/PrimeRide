88bdf17a44980b2f38f831cf7b17963c849f0d8c
ntvo110 <112120852+ntvo110@users.noreply.github.com>
2025-04-28 21:46:55 -0700

    Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,29 @@
+import classes from "./Login.module.css";
+import Logo from "../../components/logo/Logo";
+
 function Login() {
     return (
         <>
-            <div>Login</div>
+            <div className={classes.loginGrid}>
+
+                <div className={classes.imgSide}>
+                    {/* <img src="src\images\login-img.png" className={classes.loginImg}></img> */}
+                </div>
+                <div className={classes.textSide}>
+                    <div className={classes.logoSection}>
+                        <Logo/>
+                    </div>
+                    <h1>Welcome Back</h1>
+                    <p className={classes.loginText}>Log in to access your account</p>
+                    <div className={classes.input}>
+                        <input placeholder="Email Address"/>
+                    </div>
+                    <div className={classes.input}>
+                        <input placeholder="Password"/>
+                    </div>
+                    <button className={classes.loginButton}>Log in</button>
+                </div>
+            </div>
         </>
     )
 }
