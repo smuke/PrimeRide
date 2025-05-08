2e7a16189b20b73a3c20e0417e5ad422c1eb70c5
smuke <smuke@smuke.dev>
2025-05-07 19:58:59 -0700

    Update vehicle

--- src/pages/vehicle/Vehicle.tsx
+++ src/pages/vehicle/Vehicle.tsx
@@ -1,19 +1,18 @@
 import Header from "../../components/Header/Header";
 import Container from "../../components/Container/Container";
 import styles from "./Vehicle.module.css";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import vehicles from "../../data/vehicles.json";
 import locations from "../../data/locations.json"
-import StarIcon from "../../components/StarIcon/StarIcon";
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
 import users from "../../data/users.json";
 import Footer from "../../components/footer/Footer";
 import { format } from "date-fns";
-import { Separator } from "react-aria-components";
-import CardGrid from "../../components/CardGrid/CardGrid";
 import Review from "../../components/Review/Review";
 import reviews from "../../data/reviews.json";
 import Line from "../../components/Line/Line";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
 
 function Vehicle() {
     const { vehicleId } = useParams();
@@ -22,21 +21,29 @@ function Vehicle() {
 
     const location = locations.find(location => location.id == vehicle.location);
 
-    const user = users.find(user => user.id === vehicle.owner_id);
-    if (!user) return <p>User not found.</p>;
+    const owner = users.find(owner => owner.id === vehicle.owner_id);
+    if (!owner) return <p>owner not found.</p>;
 
-    const joinYear = user && new Date(user.join_date).getFullYear();
+    const joinYear = owner && new Date(owner.join_date).getFullYear();
 
-    const userReviews = reviews.filter(review => review.user_id == user.id);
+    const ownerReviews = reviews.filter(review => review.user_id == owner.id);
+
+    const {userId, setUserId} = useContext(UserContext);
+    const user = users.find(user => user.id === userId);
+    const navigate = useNavigate();
 
     const handleCheckout = async () => {
         const currentDate = new Date();
         const formattedDate = format(currentDate, "yyyy-MM-dd");
         const record = [...vehicleId, formattedDate];
         console.log(record);
-        
 
-        window.alert(vehicle.car_year + " " + vehicle.car_title + " Reserved")
+        if (!user) {
+            navigate("/login");
+            return;
+        }
+
+        window.alert(vehicle.car_year + " " + vehicle.car_title + " Reserved");
     }
 
     return (
@@ -68,12 +75,12 @@ function Vehicle() {
             <section className={styles.profileCard}>
                 <Container>
                     <ProfileCard
-                        id={user.id}
-                        name={user?.name}
-                        image={user?.image}
+                        id={owner.id}
+                        name={owner?.name}
+                        image={owner?.image}
                         joinYear={`${joinYear}`}
-                        tripCount={user?.trip_count}
-                        rating={user?.rating}
+                        tripCount={owner?.trip_count}
+                        rating={owner?.rating}
                         redirect={true}
                     />
                 </Container>
@@ -82,7 +89,7 @@ function Vehicle() {
                 <Container>
                     <h1>Reviews</h1>
                     <div className={styles.reviewContainer}>
-                        {userReviews.length > 0 ? userReviews.map((review, index) => (
+                        {ownerReviews.length > 0 ? ownerReviews.map((review, index) => (
                             <Review key={index} name={review.name} rating={review.rating} message={review.message} date={review.date} />
                         ))
                         : <p>Be the first one to leave a review!</p>}
